354ee7a951faaa13169430591ab9331b3891a2e6
Katshi Mutombo <katshi.mutombo@sanlam.co.za>
2022-07-12 18:02:22 +0200

    project init

---
+++ components/Header-comp.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar color="white">
+    <nuxt-link to="/">
+      <v-img
+        :src="require('~/assets/logo.png')"
+        max-height="24"
+        max-width="114"
+      />
+    </nuxt-link>
+
+    <v-spacer></v-spacer>
+
+    <v-list class="d-none d-sm-flex">
+      <v-list-item v-for="item in items" :key="item.title" :to="item.to">
+        <v-list-item-title class="text-center" v-text="item.title" />
+      </v-list-item>
+    </v-list>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Whats New',
+          to: '/whats-new',
+        },
+        {
+          title: 'Contact Us',
+          to: '/Contact',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from '@/components/Header-comp'
+export default {
+  name: 'DefaultLayout',
+  component: { Header },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
