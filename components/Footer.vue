a88bcf73b813906447d9bbf7be6be4ef709c9926
Katshi Mutombo <katshi.mutombo@sanlam.co.za>
2022-07-18 16:31:46 +0200

    footer component

---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-footer class="footer">
+    <v-container fluid>
+      <v-row justify="center" align="center">
+        <v-col cols="12">
+          <div class="text-center">
+            <span class="caption">Copyright &copy; 2022 Katshi Way Design</span>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss">
+.theme--light.v-footer {
+  background-color: #737373;
+  color: #fff;
+}
+</style>
